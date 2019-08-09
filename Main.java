7c7867759856a5c60bd639801bd302d8b3b5d98b
Manpreet <Manpreet@192.168.0.27>
2019-08-09 10:24:21 -0800

    Phase3- Giving user the priviledge to enter data in blockchain

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 
 public class Main {
+	
 
 	public static void main(String[] args) {
+		
+		//creating object of scanner class to take user input
+		Scanner input = new Scanner(System.in);
+		
 		// creating the first block of blockChain
 		BlockChain B1 = new BlockChain("First Block");
 
@@ -11,6 +17,7 @@ public class Main {
 
 		// After changing the data of first block the hash changes
 		B1.setData("Data Changed");
+		B1.setHash();
 		System.out.println("New Data for first Block is: " + B1.getData());
 		System.out.println("New hash Code for first Block is: " + B1.getHash());
 		
@@ -25,7 +32,6 @@ public class Main {
 		for(int i=1; i<BlockChainArray.length; i++)
 		{
 			BlockChainArray[i]= new BlockChain("",BlockChainArray[i-1].getHash());
-			
 		}
 		
 		System.out.println("Following are the Blocks created in the BlockChain: ");
@@ -33,9 +39,32 @@ public class Main {
 		//printing the data and hash codes of whole BlockChain
 		for(int i=0; i<BlockChainArray.length; i++)
 		{
-			System.out.println("Data for number "+(i)+" Block is: "+BlockChainArray[i].getData());
-			System.out.println("Hash for number "+(i)+" Block is: "+BlockChainArray[i].getHash());
+			System.out.println("Data for number "+(i+1)+" Block is: "+BlockChainArray[i].getData());
+			System.out.println("Hash for number "+(i+1)+" Block is: "+BlockChainArray[i].getHash());
 		}
+		
+		System.out.println();
+		String userInput;
+		//giving user privilege to add data in the recently created blockChain
+		for(int i=1; i<BlockChainArray.length; i++)
+		{
+			System.out.println("Enter the data for Block "+(i+1));
+			userInput= input.nextLine();
+			BlockChainArray[i].setData(userInput);
+			BlockChainArray[i].setHash(BlockChainArray[i-1].getHash());
+		}
+		
+		System.out.println();
+		System.out.println("Following is the data enterd by User with their respective hash codes:");
+		
+		//Displaying the data entered by user in the blocks of blockChain
+		for(int i=0; i<BlockChainArray.length; i++)
+		{
+			System.out.println("Data for number "+(i+1)+" Block is: "+BlockChainArray[i].getData());
+			System.out.println("Hash for number "+(i+1)+" Block is: "+BlockChainArray[i].getHash());
+		}
+		
+		input.close();
 	}
 
 }
